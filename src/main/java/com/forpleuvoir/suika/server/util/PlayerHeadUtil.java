372b5bf4be7e5c0452b99a67dc1d2a905c07f5bf
forpleuvoir <cao/4321>
2021-03-25 17:56:32 +0800

    代码优化

--- src/main/java/com/forpleuvoir/suika/server/util/PlayerHeadUtil.java
+++ src/main/java/com/forpleuvoir/suika/server/util/PlayerHeadUtil.java
@@ -1,7 +1,5 @@
 package com.forpleuvoir.suika.server.util;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.block.entity.SkullBlockEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -15,14 +13,12 @@ import net.minecraft.util.math.Vec3d;
 /**
  * @author forpleuvoir
  * @project_name suikamod
- * @package com.forpleuvoir.suika.util
+ * @package com.forpleuvoir.suika.client.util
  * @class_name PlayerHeadUtil
  * @create_time 2021/1/18 12:56
  */
 
 public class PlayerHeadUtil {
-    public static String OWNER = "SkullOwner";
-
     public static ItemStack getPlayerHead(String playerName) {
         ItemStack stack = new ItemStack(Items.PLAYER_HEAD);
         NbtCompound tag = new NbtCompound();
@@ -31,66 +27,14 @@ public class PlayerHeadUtil {
         return stack;
     }
 
-    public static ItemStack getPlayerHead(PlayerEntity player) {
-        return getPlayerHead(player.getEntityName());
-    }
-
-    public static boolean equals(ItemStack stack, ItemStack headStack) {
-        return getSkullOwner(stack).equals(getSkullOwner(headStack));
-    }
-
-    public static String getPlayerName(ItemStack stack) {
-        if (stack.hasTag()) {
-            assert stack.getTag() != null;
-            if (stack.getTag().contains("SkullOwner", 8)) {
-                System.out.println("3" + stack.getTag().getString("SkullOwner"));
-                System.out.println("4" + stack.hashCode());
-                return stack.getTag().getString("SkullOwner");
-            }
-        }
-        return "";
-    }
-
     public static ItemEntity getItemEntity(ServerWorld world, PlayerEntity player) {
         return new ItemEntity(world, player.getX(), player.getY(), player.getZ(), PlayerHeadUtil.getPlayerHead(player.getEntityName()));
     }
+
 
     public static ItemEntity getItemEntity(ServerWorld world, Vec3d pos, String name) {
         return new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), PlayerHeadUtil.getPlayerHead(name));
     }
 
-    public static String getSkullOwner(ItemStack stack) {
-        if (stack.getItem() == Items.PLAYER_HEAD && stack.hasTag()) {
-            String string = null;
-            NbtCompound compoundTag = stack.getTag();
-            assert compoundTag != null;
-            if (compoundTag.getString(OWNER).isEmpty()) {
-                if (compoundTag.contains(OWNER, 8)) {
-                    string = compoundTag.getString(OWNER);
-                } else if (compoundTag.contains(OWNER, 10)) {
-                    NbtCompound compoundTag2 = compoundTag.getCompound(OWNER);
-                    if (compoundTag2.contains("Name", 8)) {
-                        string = compoundTag2.getString("Name");
-                    }
-                }
-                if (string != null) {
-                    return string;
-                }
-            } else {
-                return compoundTag.getString(OWNER);
-            }
-        }
-        return "";
-    }
 
-    public static void loadProperties(GameProfile gameProfile, Callback callback) {
-        Thread thread = new Thread(() -> {
-            callback.callback(SkullBlockEntity.loadProperties(gameProfile));
-        });
-        thread.start();
-    }
-
-    public interface Callback {
-        void callback(Object o);
-    }
 }
